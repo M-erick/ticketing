3cc24607e2a5f1d54de457eda359d261e6e2426c
erick <muriithierick3758@gmail.com>
2024-02-26 12:02:23 +0300

    define methods ,formatteddate and to format the date

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Carbon\Carbon;
 class Event extends Model
 {
+
     use HasFactory;
+    protected $dates = ['start_datetime', 'end_datetime'];
+
+   
+
     protected $fillable =[
         'title',
         'description',
@@ -16,7 +21,34 @@ class Event extends Model
         'vip_ticket_price',
         'regular_ticket_price',
         'max_attendees',
+        'image_path',
+        'location',
+
 
     ];
-    protected $dates = ['start_datetime', 'end_datetime'];
+
+    // format the  start_time in events model
+    public function formattedDate()
+    {
+        // Check if 'start_datetime' is null
+        if ($this->start_datetime === null) {
+            return 'No start time available';
+        }
+    
+        $start_datetime = new Carbon($this->start_datetime);
+    
+        // Format the date
+        return $start_datetime->format('d F Y, H:i:s');
+    }
+    // Format the start_datetime in events model
+    public function formattedStartDate()
+    {
+        return $this->formattedDate($this->start_datetime);
+    }
+
+    // Format the end_datetime in events model
+    public function formattedEndDate()
+    {
+        return $this->formattedDate($this->end_datetime);
+    } 
 }
