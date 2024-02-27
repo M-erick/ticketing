88d34b312e696d740ad66731c10fd826e36db156
erick <muriithierick3758@gmail.com>
2024-02-27 08:32:58 +0300

    ensure max ticket booked  per person is 5

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -11,6 +11,22 @@ class ReservationController extends Controller
 {
     public function store(Request $request)
     {
+        $request->validate([
+            'user_name' => 'required|string|max:255',
+            'user_email' => 'required|email|max:255',
+            'ticket-type' => 'required|string',
+            'event' => 'required|string',
+        ]);
+
+        $userEmail = $request->input('user_email');
+        $userReservationTicketCount = Reservation::where('user_email',$userEmail)->count();
+
+        if ($userReservationTicketCount >= 5) {
+            return redirect()->back()->with('error', 'You can only reserve up to 5 tickets.');
+        }
+        
+
+        
         $ticketType = $request->input('ticket-type');
         $event = Event::where('vip_ticket_price', '>', 0)->first();
     
@@ -18,14 +34,14 @@ class ReservationController extends Controller
     
     
         Reservation::create([
-            'user_name' => $request->input('your-name'),
-            'user_email' => $request->input('your-email'),
+            'user_name' => $request->input('user_name'),
+            'user_email' => $request->input('user_email'),
             'ticket_type' => $ticketType,
             'event_title' => $request->input('event'),
         ]);
 
-            // You can redirect or respond as needed
-            return redirect()->route('events.show', $event->id)->with('success', 'Booking successful!');
+            // You can redirect back to the current page with the message
+            return redirect()->back()->with('success', 'Booking successful! Ticket Type: ' . $ticketType . ', Event Title: ' . $request->input('event'));
      
     }
 }
