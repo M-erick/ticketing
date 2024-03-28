f728fca3a5b5b12b73f6df64794d4be8cb52a3cb
erick <muriithierick3758@gmail.com>
2024-03-28 06:20:20 +0300

    first commit of the media file to incorporate real-time video conference

---
+++ resources/views/events/media.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+   
+    {{-- Embedding a youtube link onto the webpage --}}
+    <div style="overflow: hidden;padding:30px; position: relative;height:max-content; width:max-content
+    ">
+        <iframe style="left: 0; top:0;height:100%,width:100% position:absolute;" src="https://www.youtube.com/embed/VIDEO_ID" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+    
+</x-app-layout>
+
+
